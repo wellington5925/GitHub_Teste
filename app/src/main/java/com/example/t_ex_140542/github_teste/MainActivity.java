3ab07703c632b893a335252f901aa7ed6561843d
EXC66 <t-ex-140542@senacsp.edu.br>
2018-03-02 09:15:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/t_ex_140542/github_teste/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t_ex_140542.github_teste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
